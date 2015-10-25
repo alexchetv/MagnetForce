cd9e2ee9f19a348b94013f6b27f0d041b352010e
alexchetv <alexchetv@gmail.com>
2015-10-25 07:56:31 +0500

    add Savable Model MFProblem

---
+++ Shell/Models/MFProblem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.Data;
+
+namespace Shell.Models
+{
+    /// <summary>
+        /// MFProblem model which fully supports serialization, property changed notifications,
+        /// backwards compatibility and error checking.
+        /// </summary>
+    public class MFProblem : SavableModelBase<MFProblem>
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new object from scratch.
+        /// </summary>
+        public MFProblem() { }
+
+        #endregion
+
+        #region Properties
+        // TODO: Define your custom properties here using the modelprop code snippet
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Validates the field values of this object. Override this method to enable
+        /// validation of field values.
+        /// </summary>
+        /// <param name="validationResults">The validation results, add additional results to this list.</param>
+        protected override void ValidateFields(List<IFieldValidationResult> validationResults)
+        {
+        }
+
+        /// <summary>
+        /// Validates the field values of this object. Override this method to enable
+        /// validation of field values.
+        /// </summary>
+        /// <param name="validationResults">The validation results, add additional results to this list.</param>
+        protected override void ValidateBusinessRules(List<IBusinessRuleValidationResult> validationResults)
+        {
+        }
+        #endregion
+    }
+}
