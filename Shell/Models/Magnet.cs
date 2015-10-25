519dabb206333d885c0f739bd40a9208f02fc99f
alexchetv <alexchetv@gmail.com>
2015-10-25 10:07:23 +0500

    create class MagnetList create class Magnet with property Name

---
+++ Shell/Models/Magnet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.Data;
+
+namespace Shell.Models
+{
+    /// <summary>
+    /// Magnet model which fully supports serialization, property changed notifications,
+    /// backwards compatibility and error checking.
+    /// It represent one Magnet. Live or Dead
+    /// </summary>
+    public class Magnet : ModelBase
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new object from scratch.
+        /// </summary>
+        public Magnet() { }
+
+        #endregion
+
+        #region Properties
+        // TODO: Define your custom properties here using the modelprop code snippet
+        /// <summary>
+        /// Name of the magnet.
+        /// </summary>
+        public string Name
+        {
+            get { return GetValue<string>(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        /// <summary>
+        /// Register the Name property so it is known in the class.
+        /// </summary>
+        public static readonly PropertyData NameProperty = RegisterProperty("Name", typeof(string), null);
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Validates the field values of this object. Override this method to enable
+        /// validation of field values.
+        /// </summary>
+        /// <param name="validationResults">The validation results, add additional results to this list.</param>
+        protected override void ValidateFields(List<IFieldValidationResult> validationResults)
+        {
+        }
+
+        /// <summary>
+        /// Validates the field values of this object. Override this method to enable
+        /// validation of field values.
+        /// </summary>
+        /// <param name="validationResults">The validation results, add additional results to this list.</param>
+        protected override void ValidateBusinessRules(List<IBusinessRuleValidationResult> validationResults)
+        {
+        }
+        #endregion
+    }
+}
